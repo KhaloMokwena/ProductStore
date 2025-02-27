b76fcc6c01244d631854439bf41e2a6c2cc19ade
Khalo Mokwena <khalom@munisolutions.co.za>
2025-02-27 10:21:22 +0200

    Jpa Repo CRUD

--- ProductStore/src/main/java/com/khalom/crud/Product/Book.java
+++ ProductStore/src/main/java/com/khalom/crud/Product/Book.java
@@ -4,10 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +14,51 @@ public class Book {
     private String author;
     private double price;
     private String imageUrl;
+
+    public Book() {
+    }
+
+    public Book(int id) {
+        super();
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
---
+++ ProductStore/src/main/java/com/khalom/crud/Product/Repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.khalom.crud.Product.Repository;
+
+import com.khalom.crud.Product.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository {
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitleContainingIgnoreCase(String keyword);
+    List<Book> findByPriceBetween(double min, double max);
+
+}
