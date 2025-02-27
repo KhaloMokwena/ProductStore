25f6759ba9bfb2bb23663598380e654ef9e6ed01
Khalo Mokwena <khalom@munisolutions.co.za>
2025-02-27 09:37:04 +0200

    Initial commit

---
+++ ProductStore/src/main/java/com/khalom/crud/Product/Book.java
@@ -0,0 +1,19 @@
+package com.khalom.crud.Product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+    private String imageUrl;
+}
---
+++ ProductStore/src/test/java/com/khalom/crud/Product/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khalom.crud.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
